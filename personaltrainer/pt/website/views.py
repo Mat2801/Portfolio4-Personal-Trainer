241e7e6245ed137b699d940ce5471f4399dcfa9d
Mat <mathew2801@gmail.com>
2021-12-14 16:35:30 +0000

    Added map to contact page for comapny location

--- personaltrainer/pt/website/views.py
+++ personaltrainer/pt/website/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message_subject = request.POST['message-subject']
+        message = request.POST['message']
+    else:
+        return render(request, 'home.html', {})
+
