f7dc41a0ff8e5608db11a65d3d97521a9ca0bcb2
Mat <mathew2801@gmail.com>
2021-12-14 12:26:32 +0000

    installed boostrap template and starting to integrate it

--- personaltrainer/pt/pt/settings.py
+++ personaltrainer/pt/pt/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,6 +106,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# telling us where the static folder will be.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
